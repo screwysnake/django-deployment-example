61ea8fa88680e46db197118b20360b4335d2e60f
Birsan Vlad Ioan <vladioanbirsan@yahoo.com>
2021-09-23 22:06:26 +0300

    'added_learning_templates_project'

---
+++ learning_templates/basic_app/templatetags/my_extras.py
@@ -0,0 +1,12 @@
+from djanngo import template
+
+register = template.Library()
+
+@register(name = 'cut')
+def cut(value, arg):
+    """
+    This cuts out all values of "arg" from the string!
+    """
+    return value.replace(arg, '')
+
+# register.filter('cut', cut)
